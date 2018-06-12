bc1638c78be734971761f7cf1a760d702a04ada6
Annie Shin and Devin Mounts <devin@>
2018-06-12 09:35:45 -0700

    update HTML and Js for account prototypes and constructor

--- js/scripts.js
+++ js/scripts.js
@@ -23,23 +23,6 @@ Contact.prototype.fullName = function() {
 // user interface logic
 $(document).ready(function() {
 
-  // $("#add-address").click(function() {
-  //   $("#new-addresses").append('<div class="new-address">' +
-  //                                '<div class="form-group">' +
-  //                                  '<label for="new-street">Street</label>' +
-  //                                  '<input type="text" class="form-control new-street">' +
-  //                                '</div>' +
-  //                                '<div class="form-group">' +
-  //                                  '<label for="new-city">City</label>' +
-  //                                  '<input type="text" class="form-control new-city">' +
-  //                                '</div>' +
-  //                                '<div class="form-group">' +
-  //                                  '<label for="new-state">State</label>' +
-  //                                  '<input type="text" class="form-control new-state">' +
-  //                                '</div>' +
-  //                              '</div>');
-  // });
-
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -48,11 +31,11 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-account").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Account(inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
+      var inputtedInitialDeposit = $(this).find("input.initial-deposit-amount").val();
+      var inputtedDepositAmount = $(this).find("input.deposit-amount").val();
+      var inputtedWithdrawalAmount = $(this).find("input.withdrawal-amount").val();
+      var newAccount = new Account(inputtedInitialDeposit, inputtedDepositAmount, inputtedWithdrawalAmount)
+      newAccount.Account.push(newAccount)
     });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
